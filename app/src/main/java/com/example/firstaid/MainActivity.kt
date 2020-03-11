108fc8d5de5d771a0c1b0a336bdeafc0463337d8
ecoid6644 <Codeyboi6644>
2020-03-11 23:47:49 +1300

    doe some stuff my dude

--- app/src/main/java/com/example/firstaid/MainActivity.kt
+++ app/src/main/java/com/example/firstaid/MainActivity.kt
@@ -2,12 +2,18 @@ package com.example.firstaid
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log.d
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        big_button.setOnClickListener {
+            d("pitama", "button was pressed!")      // this output is found in the "Logcat section"
+        }
     }
 }
 
